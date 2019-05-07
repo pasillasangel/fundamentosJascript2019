9e86b0f1250a7cb104d68107ade251c167fe4d4c
Angel Pasillas <mpasillas@elflorido.com.mx>
2019-05-06 23:02:34 -0700

    Cambios de la tercera tanda de videos

--- clase28.js
+++ clase28.js
@@ -2,7 +2,6 @@ const API_URL = 'https://swapi.co/api/';
 
 const PEOPLE_URL = 'people/:id';
 
-const LUKE_URL = `${API_URL}${PEOPLE_URL.replace(':id', 1)}`;
 
 const opts = { crossDomain: true };
 
@@ -10,4 +9,11 @@ const onPeopleResponse = function(persona) {
     console.log(`Hola yo soy. ${persona.name}`);
 }
 
-$.get(LUKE_URL, opts, onPeopleResponse);
+function obtenerPersonaje(id) {
+    const url = `${API_URL}${PEOPLE_URL.replace(':id', id)}`;
+    $.get(url, opts, onPeopleResponse);
+}
+
+obtenerPersonaje(1);
+obtenerPersonaje(2);
+obtenerPersonaje(3);
